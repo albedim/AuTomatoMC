c6b7983d4dcc5f151b3ad8d7287b257603102dbe
Albe <albertodimaio05@gmail.com>
2025-01-25 21:14:44 +0100

    bug solving

--- src/main/java/me/albedim/automatomc/event/ChatEvent.java
+++ src/main/java/me/albedim/automatomc/event/ChatEvent.java
@@ -4,7 +4,6 @@ import me.albedim.automatomc.AuTomatoMC;
 import me.albedim.automatomc.utility.aichat.AIChat;
 import me.albedim.automatomc.utility.aichat.AIChatHandler;
 import me.albedim.automatomc.utility.chat.ChatUtility;
-import me.albedim.automatomc.utility.http.ai.AiAPI;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,12 +14,12 @@ public class ChatEvent implements Listener {
     public void onChat(org.bukkit.event.player.AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String message = event.getMessage();
-        AIChat chat = AIChatHandler.getChat(player);
-        if (chat == null) {
-            chat = AIChatHandler.createChat(player);
-        }
         if (AuTomatoMC.getInstance().AIDirectUsers.contains(player.getUniqueId())) {
             event.setCancelled(true);
+            AIChat chat = AIChatHandler.getChat(player);
+            if (chat == null) {
+                chat = AIChatHandler.createChat(player);
+            }
             ChatUtility.sendMessage(player, true, message);
             String response = AIChatHandler.addMessage(chat, message);
             ChatUtility.sendMessage(player, false, response);
